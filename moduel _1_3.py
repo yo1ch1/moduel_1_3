a0bc8f072b3e5cdbc2351dcedb74e81adb880d0e
yo1ch1 <dmit.petukhov2017@gmail.com>
2024-08-22 22:46:12 +1200

    Add files via upload

---
+++ moduel _1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = 'Дмитрий'
+print(name,(type (name)))
+age = 21
+print(age,(type (age)))
+age = 23
+print(age,(type(age)))
+is_student = True
+print(is_student,(type (is_student)))
